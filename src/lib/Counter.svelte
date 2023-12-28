670ef5b8ab6e6c919828ab677f64928591512f15
edmhok <121737342+edmhok@users.noreply.github.com>
2023-12-28 10:17:58 +0800

    reactive gotcha2

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,19 +1,24 @@
 <script>
-	let count = 0;
-	let string = `You have clicked ${count} times.`;		// let string = `You have clicked ${count} times.`;
-	
-  $: string = `You have clicked ${count} times.`;
-	
-  $: if (count > 5) {
-		console.log(`The count is ${count}`);
+  let count1 = 0;
+  let count2 = 0;
+  
+  function setCount2(x) {
+    return count2 = x;
+  }
+
+  $: setCount2(count1);
+  $: string = `Count2 is ${count2}.`;
+
+	function increment1() {
+		count1 += 1;			
 	}
-	function increment() {
-		count += 1;
-	}
+	
 </script>
 
-<button on:click={increment}>Clicks {count}</button>
+<button on:click={increment1}>Clicks {count1}</button>
+
 <h3>{string}</h3>
+
 <style>
 	button {
 		padding: 15px 20px;
